4a1acd7d0fcf1ad7ce7b051fc653a6bac913b8f0
samat <attokurov.samat@mail.ru>
2024-04-11 21:45:26 +0300

    coment

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+     gege();
+     }
+     static void gege(){
+        String name = "Пасхалка";
+         System.out.println("Я люблю тебя" + name);
+
+        }
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
